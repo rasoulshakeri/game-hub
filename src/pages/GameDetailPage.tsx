5830b8af440b3e01b2dbdacc4916d92a1bbb1d81
Rasoul <rasoulshv@gmail.com>
2023-05-15 10:44:54 +0330

    add game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom'
 import useGame from '../hooks/useGame'
-import { Heading, Spinner } from '@chakra-ui/react'
+import { GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react'
 import ExpandableText from '../components/ExpandableText'
 import GameAttributes from '../components/GameAttributes'
 import GameTrailer from '../components/GameTrailer'
+import GameScreenshots from '../components/GameScreenshots'
 
 const GameDetailPage = () => {
   const { slug } = useParams()
@@ -11,12 +12,17 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />
   if (error) throw error
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game!.description_raw}</ExpandableText>
-      <GameAttributes game={game!} />
-      <GameTrailer gameId={game!.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText>{game!.description_raw}</ExpandableText>
+        <GameAttributes game={game!} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game!.id} />
+        <GameScreenshots gameId={game!.id} />
+      </GridItem>
+    </SimpleGrid>
   )
 }
 
